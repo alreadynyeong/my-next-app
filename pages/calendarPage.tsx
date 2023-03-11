5e2bdf15b012495180655c1819e13b82b905510c
alreadynyeong <kjw87130@naver.com>
2023-03-11 19:02:13 +0900

    페이지 간 연결

---
+++ pages/calendarPage.tsx
@@ -0,0 +1,15 @@
+import { NextPage } from "next";
+import moment from "moment";
+import Link from "next/link";
+import React, {useState} from "react";
+
+
+const CalendarPage : NextPage = () => {
+  return (
+    <>
+     <p>HI CalendarPage</p>
+    </>
+  )
+}
+
+export default CalendarPage;
